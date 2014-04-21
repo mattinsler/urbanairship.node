98990fcb695f10ad3a1cc314e9e78e46c3d5297d
Matt Insler <matt.insler@gmail.com>
2014-04-21 16:59:37 -0700

    first commit

---
+++ dist/urbanairship.js
@@ -0,0 +1,90 @@
+(function() {
+  var Api, DeviceApi, Rest, UrbanAirship,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  Rest = require('rest.node');
+
+  Api = {
+    Device: DeviceApi = (function() {
+      function DeviceApi(client, id) {
+        this.client = client;
+        this.id = id;
+      }
+
+      DeviceApi.prototype.push = function(opts, cb) {
+        return this.client.post('/api/push', {
+          audience: {
+            device_token: this.id
+          },
+          device_types: 'all',
+          notification: opts
+        }, cb);
+      };
+
+      return DeviceApi;
+
+    })()
+  };
+
+  UrbanAirship = (function(_super) {
+    __extends(UrbanAirship, _super);
+
+    UrbanAirship.hooks = {
+      json: function(request_opts, opts) {
+        if (request_opts.headers == null) {
+          request_opts.headers = {};
+        }
+        return request_opts.headers.Accept = 'application/vnd.urbanairship+json; version=3';
+      },
+      auth: function(key, secret) {
+        return function(request_opts, opts) {
+          console.log('auth', {
+            username: key,
+            password: secret
+          });
+          return request_opts.auth = {
+            username: key,
+            password: secret
+          };
+        };
+      },
+      json_body: function(request_opts, opts) {
+        return request_opts.json = opts;
+      }
+    };
+
+    UrbanAirship.prototype.parse_response_body = function(headers, body) {
+      if (typeof body !== 'string') {
+        return body;
+      }
+      if (!(body[0] === '{' && body[body.length - 1] === '}')) {
+        return body;
+      }
+      return JSON.parse(body);
+    };
+
+    function UrbanAirship(options) {
+      this.options = options != null ? options : {};
+      UrbanAirship.__super__.constructor.call(this, {
+        base_url: this.options.base_url || 'https://go.urbanairship.com'
+      });
+      console.log(this.options);
+      this.hook('pre:request', UrbanAirship.hooks.json);
+      if ((this.options.key != null) && (this.options.secret != null)) {
+        this.hook('pre:request', UrbanAirship.hooks.auth(this.options.key, this.options.secret));
+      }
+      this.hook('pre:post', UrbanAirship.hooks.json_body);
+    }
+
+    UrbanAirship.prototype.device = function(id) {
+      return new Api.Device(this, id);
+    };
+
+    return UrbanAirship;
+
+  })(Rest);
+
+  module.exports = UrbanAirship;
+
+}).call(this);
---
+++ lib/urbanairship.coffee
@@ -0,0 +1,41 @@
+Rest = require 'rest.node'
+
+Api = {
+  Device: class DeviceApi
+    constructor: (@client, @id) ->
+    push: (opts, cb) -> @client.post('/api/push', {audience: {device_token: @id}, device_types: 'all', notification: opts}, cb)
+}
+
+class UrbanAirship extends Rest
+  @hooks:
+    json: (request_opts, opts) ->
+      request_opts.headers ?= {}
+      # request_opts.headers.Accept = 'application/json'
+      request_opts.headers.Accept = 'application/vnd.urbanairship+json; version=3'
+    
+    auth: (key, secret) ->
+      (request_opts, opts) ->
+        console.log('auth', {username: key, password: secret})
+        request_opts.auth =
+          username: key
+          password: secret
+    
+    json_body: (request_opts, opts) ->
+      request_opts.json = opts
+  
+  parse_response_body: (headers, body) ->
+    return body unless typeof body is 'string'
+    return body unless body[0] is '{' and body[body.length - 1] is '}'
+    JSON.parse(body)
+  
+  constructor: (@options = {}) ->
+    super(base_url: @options.base_url or 'https://go.urbanairship.com')
+    
+    console.log @options
+    @hook('pre:request', UrbanAirship.hooks.json)
+    @hook('pre:request', UrbanAirship.hooks.auth(@options.key, @options.secret)) if @options.key? and @options.secret?
+    @hook('pre:post', UrbanAirship.hooks.json_body)
+  
+  device: (id) -> new Api.Device(@, id)
+
+module.exports = UrbanAirship
